880e9a77dfa98a99e834c3753ebe29a9f02bf289
Juwan Petty <juwan.petty@shopify.com>
2018-11-15 16:47:10 -0800

    Add initial files

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,14 @@
+import React, { PureComponent } from 'react'
+import Link from 'gatsby-link'
+
+import styles from './Navigation.module.scss'
+
+class Navigation extends PureComponent {
+  // methods
+
+  render() {
+    //render something
+  }
+}
+
+export default Navigation
